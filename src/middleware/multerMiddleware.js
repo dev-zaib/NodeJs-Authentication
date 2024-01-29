ffbfe2230903778217ff5b22bb6238c8ae62e350
dev-zaib <devzaib01@gmail.com>
2024-01-29 15:51:44 +0500

    Handle file uploading using Multer & Cloudinary

---
+++ src/middleware/multerMiddleware.js
@@ -0,0 +1,10 @@
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./Public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage })
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary= async ()=>{
+    try {
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("Full has been uploaded successfully", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+    }
+}
